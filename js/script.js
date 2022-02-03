25dc4f2358d2ae191a23b4e0dff4e7555e1616d9
jonnebonde <jonnebonde@gmail.com>
2022-02-03 10:10:31 +0100

    enter

--- js/script.js
+++ js/script.js
@@ -48,3 +48,32 @@ button.onclick = function() {
 myObject.someFunction(); */
 
 
+
+// remainder operator - %
+// sometimes referred to as the modulus operator 
+
+const remainder = 8 % 3;
+
+console.log(remainder);
+
+
+const table = document.querySelector("table");
+
+for(let i = 1; i <= 10; i++) {
+
+
+    let className = "";
+
+    if(i % 2 === 0) {
+        className = "highlight";
+    }
+
+
+
+    table.innerHTML += `<tr class="${className}">
+                        <td>${i}</td>
+                        <td>Text content</td>
+                        </tr>`;
+}
+
+
